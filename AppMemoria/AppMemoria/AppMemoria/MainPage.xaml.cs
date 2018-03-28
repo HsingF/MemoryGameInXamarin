14bd3043362ffd04eb33631c0595b559dc184f97
HsingF <30559956+HsingF@users.noreply.github.com>
2018-03-28 01:17:30 -0600

    Add files via upload
    Project

---
+++ AppMemoria/AppMemoria/AppMemoria/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppMemoria
+{
+	public partial class MainPage : ContentPage
+	{
+        private MatrizMemoria mm;
+        private Button primero;
+        private Button segundo;
+
+		public MainPage()
+		{
+            mm = new MatrizMemoria();
+            mm.GenerarMatriz();
+            primero = null;
+            segundo = null;
+			InitializeComponent();
+		}
+
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (primero == null)
+            { // there is no button selected yet
+                primero = button;
+                primero.Text = GetByPosition(button);
+                primero.BackgroundColor = Color.YellowGreen;
+            }
+            else if (segundo == null)
+            { // there is only one button selected
+                if (primero != button)
+                {
+                    segundo = button;
+                    segundo.Text = GetByPosition(button);
+                    segundo.BackgroundColor = Color.YellowGreen;
+                }
+            }
+            else
+            { // there are 2 buttons selected
+                if (CheckMatch() && (button == primero || button == segundo)) // true if there was a match
+                    primero = null;
+                else // wasnt a match
+                {
+                    primero = button;
+                    primero.Text = GetByPosition(button);
+                    primero.BackgroundColor = Color.YellowGreen;
+                }
+                segundo = null;
+            }
+        }
+
+        async void PrintClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Answer",mm.GetAnswer(), "CLOSE");
+        }
+
+        public string GetByPosition(Button b)
+        {
+            int r1 = (int)Char.GetNumericValue(b.AutomationId[0]);
+            int c1 = (int)Char.GetNumericValue(b.AutomationId[1]);
+            return mm.GetByPosition(r1,c1);
+        }
+
+        public bool CheckMatch() // true if there is a match between primero and segundo
+        {
+            int r1 = (int)Char.GetNumericValue(primero.AutomationId[0]);
+            int c1 = (int)Char.GetNumericValue(primero.AutomationId[1]);
+            int r2 = (int)Char.GetNumericValue(segundo.AutomationId[0]);
+            int c2 = (int)Char.GetNumericValue(segundo.AutomationId[1]);
+            if (mm.GetByPosition(r1, c1) == mm.GetByPosition(r2, c2)) // match
+            {
+                primero.BackgroundColor = Color.Gray;
+                segundo.BackgroundColor = Color.Gray;
+                primero.IsEnabled = false;
+                segundo.IsEnabled = false;
+                return true;
+            }
+            else //no match
+            {
+                primero.BackgroundColor = Color.Orange;
+                segundo.BackgroundColor = Color.Orange;
+                primero.Text = " ";
+                segundo.Text = " ";
+                return false;
+            }
+        }
+        
+    }
+}
---
+++ AppMemoria/AppMemoria/AppMemoria/MatrizMemoria.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppMemoria
+{
+    class MatrizMemoria
+    {
+        private string[,] matriz;
+        public const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private static Random random;
+
+        public MatrizMemoria()
+        {
+            matriz = new string[5,5];
+            random = new Random();
+        }
+
+        public static Random GetRandom()
+        {
+            return random;
+        }
+
+        public static string RandomString()
+        {
+            int index = GetRandom().Next(alphabet.Length);
+            return alphabet[index] + "";
+        }
+
+        public string GetByPosition(int i, int j)
+        {
+            return matriz[i,j];
+        }
+
+        public string GetAnswer()
+        {
+            string result="";
+            for(int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    result += GetByPosition(i,j) + "\t\t";
+                }
+                result += "\r\n";
+            }
+            return result;
+        }
+
+        public void GenerarMatriz()
+        {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    matriz[i,j] = RandomString();
+                }
+            }
+        }
+    }
+}
